1d4bb20436af86a1759dff41a9029d019f3f4c7b
flo <flo.liang@my.com>
2015-06-12 23:46:48 -0700

    Finish login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
+        log_in @user
         flash[:success] = "Welcome to Pets Home!"
         redirect_to @user
       else
