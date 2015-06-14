415dc1b319629f3bb3a6c61e8f8e0b7e33aa10a1
flo <flo.liang@my.com>
2015-06-14 14:26:55 -0700

    Complete showing and editing users profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Users
+
+ 99.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10),
+     admin: true
+   )
+   user.save!
+ end
+ users = User.all
+
+ user = User.first
+ # user.skip_reconfirmation!
+ user.update_attributes!(
+   name: 'user1',
+   email: 'user1@example.com',
+   password: 'foobar'
+ )
+
+ puts "Seed finished"
+ puts "#{User.count} users created"
