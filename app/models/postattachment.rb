29d5658be9833d608b53d61b99c0622fe527b3f0
floalex <flo.liang@my.com>
2015-10-12 20:05:13 -0700

    Add picture size validations and aws

--- app/models/postattachment.rb
+++ app/models/postattachment.rb
@@ -1,4 +1,13 @@
 class Postattachment < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   belongs_to :post
+  validate :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 3MB")
+    end
+  end
 end
