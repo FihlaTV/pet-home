6bd0fe5bab91fb3c452023eac43693d09b79068a
floalex <flo.liang@my.com>
2015-10-29 23:48:26 -0700

    Add gravatar to head

--- db/schema.rb
+++ db/schema.rb
@@ -57,21 +57,6 @@ ActiveRecord::Schema.define(version: 20151006192229) do
 
   add_index "postattachments", ["post_id"], name: "index_postattachments_on_post_id"
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "category_id"
-    t.integer  "user_id"
-    t.integer  "location_id"
-    t.string   "slug"
-  end
-
-  add_index "posts", ["category_id"], name: "index_posts_on_category_id"
-  add_index "posts", ["location_id"], name: "index_posts_on_location_id"
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
