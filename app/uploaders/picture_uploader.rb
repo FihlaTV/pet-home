d1be3f5d275ce01b5ac6492f873a8c00dccf128d
floalex <flo.liang@my.com>
2015-11-21 12:14:28 -0800

    Add pictures to post show view

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,7 +5,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_limit: [200, 300]
+  process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
@@ -38,6 +38,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :thumb do
     process resize_to_fit: [100, 100]
+  end
+
+  version :large do
+    process resize_to_fit: [600, 600]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
