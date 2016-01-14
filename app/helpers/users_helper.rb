3606876e306735d274008720c63bdeda9616ee41
floalex <flo.liang@my.com>
2016-01-14 13:46:45 -0800

    Add facebook authentication

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,11 @@
 module UsersHelper
-  def gravatar_for(user, options = { size: 164 })
+  def gravatar_url(user, options = { size: 164 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     if user.image
-      gravatar_url = user.image
+      user.image
     else
-      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+      "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     end
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112035326) do
+ActiveRecord::Schema.define(version: 20160114170623) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -91,6 +91,8 @@ ActiveRecord::Schema.define(version: 20160112035326) do
     t.string   "image"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.string   "phone_number"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
